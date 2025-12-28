092b1b36aeef28011a6fb3b7318762a89696403e
Samir Singh <samir@antino.com>
2025-12-28 13:05:50 +0530

    type inference done

---
+++ 8-Type Inference/index.ts
@@ -0,0 +1,12 @@
+/*
+Type inference in Typescript refers to the ability of the Typescript compiler to automatically determine and assign types to variables, expressions, and function return values based on their usage and context in the code.
+
+Q: What are some best practices for using type inference ?
+-> Used type inference for simple cases where the assigned value clearly indicates the intended value.
+-> When in doubt, provide explicit type annotations to make your intentions clear.
+-> Avoid relying too heavily on type inference when the assigned value is complex or ambiguous.
+
+Q: D/b type annotation and type inference ?
+-> type annotation means we are explicitly specify the type of the variable, function or object.
+-> type inference means the compiler automatically specify the type of the variable, function on the basis of the context or the block of code written.
+*/
